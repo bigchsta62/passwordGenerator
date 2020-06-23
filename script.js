b11c981e6fb17d4ae3194abd333f7e7b24491ef7
bigchsta62 <bigchsta62@msn.com>
2020-06-23 00:49:29 -0700

    Finally Worksgit add -A Gave page personal touches

--- script.js
+++ script.js
@@ -1,51 +1,3 @@
-// # 03 JavaScript: Password Generator
-
-// Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
-
-// If you are unfamiliar with special characters, take a look at [some examples](https://www.owasp.org/index.php/Password_special_characters).
-
-// ## User Story
-
-// ```
-// AS AN employee with access to sensitive data
-// I WANT to randomly generate a password that meets certain criteria
-// SO THAT I can create a strong password that provides greater security
-// ```
-
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I need a new, secure password
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// ```
-
-// The following image demonstrates the application functionality:
-
-// ![password generator demo](./Assets/03-javascript-homework-demo.png)
-
-// ## Review
-
-// You are required to submit the following for review:
-
-// * The URL of the deployed application.
-
-// * The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-// - - -
-// © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
-
 
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
@@ -58,58 +10,94 @@ const lCase = "abcdefghijklmnopqrstuvwxyz";
 const nums = "123456789";
 const sChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-// const pwdLength = '';
-
 let randomPwd = [];
 let pwdInput = [];
 
+
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
-function createPwd() {
-  const pwdLength = prompt ("How many character would you like your password to be?");
-  
-  // while (pwdLength < 8 || pwdLength > 128) {
-  //   alert ("Your password must be between 8 and 128 characters");
-  //   const pwdLength = (prompt ("How many character would you like your password to be?"));
-  // }
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
 
-  const upperCase = confirm ('Would you like Uppercase letters in your password?');
-  const lowerCase = confirm ('Would you like Lowercase letters in your password?');
-  const numbers = confirm ('Would you like Numbers in your password?');
-  const specialChar = confirm ('Would you like Uppercase letters in your password?');
+function generatePassword() {
+	var pwdLength = (prompt("How many character would you like your password to be?"));
 
-if (upperCase) {
-  randomPwd = randomPwd.concat(uCase.split(''));
-  // console.log(randomPwd);
-}
-if (lowerCase) {
-  randomPwd = randomPwd.concat(lCase.split(''));
-  // console.log(randomPwd);
-}
-if (numbers) {
-  randomPwd = randomPwd.concat(nums.split(''));
-  // console.log(randomPwd);
-}
-if (specialChar) {
-  randomPwd = randomPwd.concat(sChar.split(''));
-  // console.log(randomPwd);
-}
+	// THEN I choose a length of at least 8 characters and no more than 128 characters
+	// 'while' used instead of 'if'. This kept the minimum requirements from being bypassed.
+	while (pwdLength < 8 || pwdLength > 128) {
+		alert("Your password must be between 8 and 128 characters");
+		var pwdLength = (prompt("How many character would you like your password to be?"));
+	}
 
-for (let i = 0; i < pwdLength; i++) {
-  pwdInput.push(randomPwd[Math.floor(Math.random() * randomPwd.length)]);
-  
-}
-return pwdInput.join('');
+	// WHEN prompted for character types to include in the password
+	// THEN I choose lowercase, uppercase, numeric, and/or special characters
 
+	let upperCase = confirm('Would you like Uppercase letters in your password?');
+	let lowerCase = confirm('Would you like Lowercase letters in your password?');
+	let numbers = confirm('Would you like Numbers in your password?');
+	let specialChar = confirm('Would you like Uppercase letters in your password?');
+
+	// WHEN I answer each prompt
+	// THEN my input should be validated and at least one character type should be selected
+
+	while (upperCase === false && lowerCase === false && specialChar === false && numbers === false) {
+		alert("At least one type of character must be selected");
+		let upperCase = confirm('Would you like Uppercase letters in your password?');
+		let lowerCase = confirm('Would you like Lowercase letters in your password?');
+		let numbers = confirm('Would you like Numbers in your password?');
+		let specialChar = confirm('Would you like Uppercase letters in your password?');
+	}
+	//If selected, includes uppercase letters in the password
+	if (upperCase) {
+		randomPwd = randomPwd.concat(uCase.split(''));
+		// console.log(randomPwd);
+	}
+
+	//If selected, includes lowercase letters
+	if (lowerCase) {
+		randomPwd = randomPwd.concat(lCase.split(''));
+		// console.log(randomPwd);
+	}
+
+	//If selected, includes numbers
+	if (numbers) {
+		randomPwd = randomPwd.concat(nums.split(''));
+		// console.log(randomPwd);
+	}
+
+	//If selected, includes special characters
+	if (specialChar) {
+		randomPwd = randomPwd.concat(sChar.split(''));
+		// console.log(randomPwd);
+	}
+	//pushes the selected variables, at random into pwdInput,
+	//at the length of randomPwd.
+	for (let i = 0; i < pwdLength; i++) {
+		pwdInput.push(randomPwd[Math.floor(Math.random() * randomPwd.length)]);
+
+	}
+	// WHEN all prompts are answered
+	// THEN a password is generated that matches the selected criteria
+	
+	return pwdInput.join('');
+	// console.log(pwdInput.join(''));
+	// WHEN the password is generated
+	// THEN the password is either displayed in an alert or written to the page
 }
 
 function writePassword() {
-  const password = createPwd();
-  const passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-  }
-  alert('here is your password' + ' ' + password)
-
-
-
+	let password = generatePassword();
+	let passwordText = document.querySelector("#password");
+	passwordText.value = password;
+}
+// This function disable the start button, once the code is generated
+// This prevents the code from running again until the page is refreshed
+function save() {
+	const msg = document.getElementById('msg');
+	msg.innerHTML = 'Purge';
+}
+// This enables the Purge button to refresh the page, resetting the form.
+function reset() {
+	window.location.reload();
+}
